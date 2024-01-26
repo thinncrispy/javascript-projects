bdf2218ed66fc5acfb47048a440666c08bb7f369
thinncrispy <wilson.hayward@gmail.com>
2024-01-25 19:00:06 -0600

    wooeee

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,61 @@ function makeRectangle(width, height) {
       rectangle += (makeLine(width) + '\n');
     }
     return rectangle.slice(0, -1);
-  }
+}
 
 console.log(makeRectangle(3,7));
+
+function makeDownwardStairs(height) {
+    let stairs = '';
+    for (let i = 0; i < height; i++) {
+        stairs += (makeLine(i+1) + '\n');
+    }
+    return stairs.slice(0, -1);
+}
+
+console.log(makeDownwardStairs(6));
+
+function makeSpaceLine(numSpaces, numChars) {
+    let line = '';
+  
+    // Add spaces
+    for (let i = 0; i < numSpaces; i++) {
+      line += ' ';
+    }
+  
+    // Add hashes
+    for (let i = 0; i < numChars; i++) {
+      line += '#';
+    }
+  
+    // Add spaces after hashes
+    for (let i = 0; i < numSpaces; i++) {
+      line += ' ';
+    }
+  
+    return line;
+}
+  
+console.log(makeSpaceLine(2, 5)); // Output: "  #####  "
+  
+
+function makeIsoscelesTriangle(height){
+    let triangle = '';
+    for (let i = 0; i < height; i++){
+        triangle += (makeSpaceLine(height - i - 1, 2 * i + 1) + '\n');
+    }
+    return triangle.slice(0,-1);
+}
+
+console.log(makeIsoscelesTriangle(4));
+
+function reverse(s){
+    return s.split('').reverse().join('');
+}
+
+function makeDiamond(height) {
+    let diamond = makeIsoscelesTriangle(height);
+    diamond += ('\n' + reverse(diamond.slice()));
+    return diamond;
+}
+console.log(makeDiamond(3));
