605bda5778efb523335d7ead274d4c4648baaaab
thinncrispy <wilson.hayward@gmail.com>
2024-02-08 20:28:55 -0600

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,23 @@
 
 let launchcode = {
-  
+  oragnization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(any){
+    if(any % 2 === 0){
+        return "Launch!";
+    } else if(any % 3 === 0){
+        return "Code!";
+    } else if(any % 5 === 0){
+        return "Rocks!";
+    }
+
+  }
 }
+
+
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,35 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("type of organization should be non profit", function(){
+    expect(launchcode.oragnization).toBe("nonprofit");
+  })
+  test("the director should be Jeff", function(){
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  })
+  test("The percentage of cool employees should be 100", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  })
+  test("programs Array testing", function(){
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  })
+});
 
+  describe("testing Launchoutput", function(){
+  test("divisible by 2", function(){
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  })
+  test("only divisible by 3", function(){
+    expect(launchcode.launchOutput(9)).toBe("Code!");
+    expect(launchcode.launchOutput(15)).toBe("Code!");
+  })
+  test("only divisible by 5", function(){
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+    expect(launchcode.launchOutput(15)).toBe("Rocks!");
+  })
   // Write your unit tests here!
   
 });
